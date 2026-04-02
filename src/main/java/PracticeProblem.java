078ce7825f2071948e2b6cb84767fa2f9c7f959c
Leorix17 <343430617@tdsb.ca>
2026-04-02 14:45:33 +0000

    fin

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,33 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		
+	}
+
+	public static String evenOrOdd(int num) {
+		if ((num % 2) == 0) {
+			return "Even";
+		}
+		return "Odd";
 
 	}
 
+	public static String teacherOrStudent(String word) {
+		if (word == "Kalisz") {
+			return "Teacher";
+		}
+		return "Student";
+	}
+
+	public static int fartherFromZero(int num) {
+		if (num > 0) {
+			num = num + 5;
+			return num;
+		}
+		if (num < 0) {
+			num = num - 5;
+			return num;
+		}
+		return 0;
+	}
 }
